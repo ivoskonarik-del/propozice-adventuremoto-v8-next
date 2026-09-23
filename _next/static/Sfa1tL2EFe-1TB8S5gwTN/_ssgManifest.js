9c6c4c6c0edf88b2de79f85071f3c3199193ed70
Weboviny Deploy <deploy@weboviny.cz>
2026-09-23 18:18:02 +0200

    Deploy 2026-09-23

---
+++ _next/static/Sfa1tL2EFe-1TB8S5gwTN/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fvypravy\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
---
+++ _next/static/chunks/app/obchodni-podminky/page-67004ff7108a5b43.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[911,990],{3998:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,8173,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[173,441,517,358],()=>s(3998)),_N_E=e.O()}]);
